7ad3e43dca91d1d05bbe19902d3b0284024d5111
a i <wwwzlx86@gmail.com>
2021-09-28 07:22:57 +0300

    1

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,34 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("123412341234123");
+            // задание 1!!!
+            Console.WriteLine("exercise 1");
+            Console.WriteLine();
+            for (var i =-100; i<50; i+=5)
+            {
+                Console.WriteLine(i + 5);
+            }
+
+            //задание 2
+            Console.WriteLine();
+            Console.WriteLine("exercise 2");
+            Console.WriteLine();
+            for (var i =0; i<5; i++)
+            {
+                char simbol = 'M';
+                Console.WriteLine(simbol);
+            }
+            //задание 3
+            Console.WriteLine();
+            Console.WriteLine("exercise 3");
+            Console.WriteLine();
+            for (var i = 0; i < 4; i++) ;
+            {
+                string j = "#";
+                Console.WriteLine(j);
+
+            }
+
         }
     }
 }
