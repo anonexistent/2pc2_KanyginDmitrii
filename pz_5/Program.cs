b71a705d80dfeb49aeaf1c664a98366afc2927f3
a i <wwwzlx86@gmail.com>
2021-09-28 07:48:08 +0300

    1

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -18,22 +18,50 @@ namespace pz_5
             Console.WriteLine();
             Console.WriteLine("exercise 2");
             Console.WriteLine();
+            char s111 = 'M';
             for (var i =0; i<5; i++)
             {
-                char simbol = 'M';
-                Console.WriteLine(simbol);
+                Console.WriteLine(s111);
+                s111++;
             }
+
             //задание 3
             Console.WriteLine();
             Console.WriteLine("exercise 3");
             Console.WriteLine();
-            for (var i = 0; i < 4; i++) ;
+            var j1 = '#';
+            for (int i = 0; i != 4; i++) 
             {
-                string j = "#";
-                Console.WriteLine(j);
-
+                for(int i0 = 0; i0!=6; i0++)
+                {
+                    Console.Write(j1);
+                }
+                Console.WriteLine();
             }
 
+            //exercise 4
+            Console.WriteLine();
+            Console.WriteLine("exercise 4");
+            Console.WriteLine();
+            for (int i = 1; i!=100; i++)
+            {
+                var cc = i % 7;
+                if(cc==0 )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //exercise 5
+            Console.WriteLine();
+            Console.WriteLine("exercise 5");
+            Console.WriteLine();
+            int j = 40;
+            for(int i=1; ; i++)
+            {
+                Console.WriteLine(i + " " + j);
+                j--;
+            }
         }
     }
 }
