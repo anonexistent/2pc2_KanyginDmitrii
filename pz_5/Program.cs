3ecb2e72bbdd965bc37f611860c56c116060398a
a i <wwwzlx86@gmail.com>
2021-09-28 06:52:59 +0300

    12

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,7 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("123412341234123");
         }
     }
 }
