887530cc2aae5ad23f9fb388e9c5b41e0ee5f406
a i <wwwzlx86@gmail.com>
2021-09-29 11:49:13 +0300

    1

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -6,7 +6,24 @@ namespace pz_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ulong n, n1 = 0;
+            n1 = n = ulong.Parse(Console.ReadLine());
+            int y;
+            y = 
+            if (n % 2 == 1)
+            {
+                y = 1;
+            }
+            else
+            {
+                y = 2;
+            }
+            while (n != y)
+            {
+                n -= 2;
+                n1 *= n;
+            }
+            Console.WriteLine(n1);
         }
     }
 }
