0cc6a5d74c3e762aeb824bd51f032b2ab7f72df7
a i <wwwzlx86@gmail.com>
2021-09-29 11:54:35 +0300

    1

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -10,14 +10,6 @@ namespace pz_6
             n1 = n = long.Parse(Console.ReadLine());
             int y;
             y = (n %2== 1) ? 1 : 2;
-            //if (n % 2 == 1)
-            //{
-            //    y = 1;
-            //}
-            //else
-            //{
-            //    y = 2;
-            //}
             while (n != y)
             {
                 n -= 2;
