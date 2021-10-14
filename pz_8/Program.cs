9e71c9db869366036a4501808c9c3e5e564e7116
Дмитрий Каныгин <wwwzlx86@gmail.com>
2021-10-14 11:51:16 +0300

    1

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -7,32 +7,36 @@ namespace pz_8
         static void Main(string[] args)
         {
             //вариант 12
-            int x = 0;
-            Console.WriteLine("size: ");
-            var N = Byte.Parse(Console.ReadLine());
-            Console.WriteLine("even num: ");
-            byte[] ar = new byte[N];
-            byte[] ar2 = new byte[];
+            int j = 0;
+            Console.WriteLine("size: "); 
+            var N = Int32.Parse(Console.ReadLine()); // размер начального массива
+            int[] ar = new int[N]; //начальный массив и массив четных чисел
+            int[] ar2 = new int[N];
             Random uuu = new Random();
-            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("array: ");
+            Console.BackgroundColor = ConsoleColor.White; 
             Console.ForegroundColor = ConsoleColor.Red;
-            for (int i = 0; i < ar.Length; i++)
+            for (int i = 0; i < ar.Length; i++) //запоняем массив
             {
-                ar[i] = (byte)uuu.Next();
+                ar[i] = uuu.Next();
                 Console.WriteLine(ar[i]);
             }
+            Console.ResetColor();
+            Console.WriteLine("even num: ");
             Console.BackgroundColor = ConsoleColor.Magenta;
             Console.ForegroundColor = ConsoleColor.White;
-            for (int i = 0; i != ar.Length; i++)
+            for (int i = 0; i != ar.Length; i++) // проходим по массиву ии
             {
-                if (ar[i] % 2 == 0)
+                if (ar[i] % 2 == 0) //и выбираем четные
                 {
                     ar2[i] = ar[i];
-                    Console.WriteLine(ar2[i]);
+                    Console.WriteLine(ar2[i]); //вывод%% чентные числа
+                    j++; //счетчик
                 }
             }
             Console.ResetColor();
-            Console.WriteLine($"amount: {ar2.Length}");
-        }
+            Console.WriteLine($"amount: {j}"); //вывод%% кол во четных чисел
+            Console.ReadLine();       
+        }                      
     }
 }
