46085bfcfbf6e51728c818e152621276476a20c8
Дмитрий Каныгин <wwwzlx86@gmail.com>
2021-10-15 12:11:06 +0300

    1

--- pz_9/Program.cs
+++ pz_9/Program.cs
@@ -9,12 +9,15 @@ namespace pz_9
             Console.BackgroundColor = ConsoleColor.Magenta;
             Console.ForegroundColor = ConsoleColor.White;
             int x, y, z;
-            x = y = z = 0;
+            x = y = z = 0;                              
             sbyte[,] ar = new sbyte[8, 6];
             sbyte[] ar0 = new sbyte[48];
             Random uuu = new Random();
-            for (int i=0; i!=8;i++)
-            {
+            //пробегаемся  по массиву ar: выводим его, добавляем элементы в доп массив,
+            //чтоб потом сложить(y) и находи минимальный элемент массива(z)
+                for (int i=0; i != 8; i++) 
+                {        
+                
                 for(int j=0; j!=6;j++)
                 {
                     ar[i, j] = (sbyte)uuu.Next(sbyte.MinValue, sbyte.MaxValue);
@@ -26,21 +29,20 @@ namespace pz_9
                     }
                     else if (ar[i, j] < z)
                     {
-                        z = ar[i,j];            //z - минимальный элемент массива ar
+                        z = ar[i,j];            
                     }                  
                 }
                 Console.WriteLine();
-            }
+                }
             Console.BackgroundColor = ConsoleColor.Cyan;
             Console.ForegroundColor = ConsoleColor.Black;
-            while (x!=0)                //у - сумма "+элементов"
+            //у - сумма "+элементов"
+            while (x!=0)                
             {
                 y += ar0[x];
                 x--;
             }
             Console.WriteLine($"Ответ: {y*z}");
-
-
 
             Console.ResetColor();
             Console.ReadLine();
