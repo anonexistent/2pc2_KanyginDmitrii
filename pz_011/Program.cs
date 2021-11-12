856d175daab6cf1639a628619ddc8c505454c740
Дмитрий Каныгин <wwwzlx86@gmail.com>
2021-11-12 03:09:36 +0300

    1

---
+++ pz_011/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void print(object a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(a);
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+
+            char c = Convert.ToChar(Console.ReadLine());
+            char[] s = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == c)
+                {
+                    Array.Resize(ref s, s.Length + 1);
+                    for (int j = s.Length - 2; j >= i; j--)
+                    {
+                        s[j + 1] = s[j];
+                    }
+                    i++;
+                }
+            }
+            for (int i = 0; i < s.Length; i++)
+                print(s[i]);
+
+        }
+    }
+}
---
+++ pz_012/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace pz_012
+{
+    class Program
+    {
+        static void print(object a)
+        {   Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(a);
+            Console.ResetColor();
+        }
+        //static void Main(string[] args)                 //вар 1 пз 12
+        //{
+        //    string str = Console.ReadLine(), str0 = " ";
+        //    string[] arr = str.Split(". ");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (i==0 | i==arr.Length-1)
+        //        {
+        //            str0 = arr[0];
+        //            arr[0] = arr[i];
+        //            arr[i] = str0;
+        //        }
+        //    }
+        //    for (int i = 0; i < arr.Length; i++)
+        //        print(arr[i]+". ");
+
+        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("text: ");
+
+            string str = Console.ReadLine();
+
+            string[] arr = str.Split('.');
+
+            string a;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].Length > arr[j].Length)
+                    {
+                        a = arr[i];
+
+                        arr[i] = arr[j];
+
+                        arr[j] = a;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+                print(arr[i]+' ');
+        }
+    }
+}
