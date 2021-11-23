59e74b61e000e8fe1d32663d7485513d46ba6488
Дмитрий Каныгин <wwwzlx86@gmail.com>
2021-11-23 23:05:37 +0300

    1

---
+++ ConsoleApp))0)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp__0_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("pls enter file name pls: ");
+
+                FileStream file1 = new FileStream(new string(@"C:\fet\" + @$"{Console.ReadLine()})"), FileMode.Append);
+
+                string dirrr = @"C:\fet\";
+
+                DirectoryInfo dirinfo = new DirectoryInfo(dirrr);
+
+                dirinfo.
+
+                FileStream file0 = new FileStream(@"C:\fet\no.txt", FileMode.Append);
+
+                string[] str0 = Console.ReadLine().Split('.');
+
+                StreamWriter writer = new StreamWriter(file0);
+
+                //65-90 upper
+                //97-122 down
+
+                //for (int i = 0; i < str0.Length-1; i++)
+                //{
+                // if (str0[i-1].EndsWith('.')) char.ToUpper(str0[i][0]);
+                //}
+
+                //for (int i = 0; i < str0.Length; i++)
+                //{
+                // Console.WriteLine(str0[i]);
+                //}
+
+                //for (int i = 0; i < str0.Length; i++)
+                //{
+                // char.ToUpper(str0[i][0]);
+                // Console.WriteLine(str0[i]);
+                //}
+
+                for (int i = 0; i < str0.Length; i++)
+                {
+                    writer.Write(str0[i]);
+                }
+
+                writer.WriteLine("\n\n");
+                writer.Close();
+        }
+    }
+}
---
+++ pz_014/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace pz_014
+{
+    class Program
+    {
+        static void print(object a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(a);
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+            FileInfo[] aboutfouler = new DirectoryInfo(@"C:\fet\").GetFiles();
+
+            print("pls choose .txt file pls (number):");
+
+            for (int i = 0; i < aboutfouler.Length; i++) Console.Write($"\t{i}) " + aboutfouler[i].ToString().Substring(7) + "\n");
+
+            int filenumber = 0;
+
+            do
+            {
+                filenumber = int.Parse(Console.ReadLine());
+            } while (aboutfouler[filenumber].Extension != ".txt");
+
+            if (aboutfouler[filenumber].Extension != ".txt") Console.WriteLine("mb u can to choose another file?");
+            else
+            {
+                FileStream file1 = new FileStream(new string($"{aboutfouler[filenumber]}"), FileMode.Append);
+
+                print("what to add to file");
+
+                char[] strs = Console.ReadLine().ToCharArray();
+
+                char res = ' ';
+
+                int charindex = 0;
+
+                strs[0] = char.ToUpper(strs[0]);
+
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (strs[i] == '.')
+                    {
+                        charindex = i;
+                        res = strs[i];
+                    }
+                    if (i == charindex + 2) strs[i] = Char.ToUpper(strs[i]);
+                }
+
+                StreamWriter writer = new StreamWriter(file1);
+
+                writer.Write(string.Join('\0', strs));
+                writer.Close();
+            }
+        }
+    }
+}
