4c465b325484b24bbe47cdcc5374c39565e4b311
Дмитрий Каныгин <wwwzlx86@gmail.com>
2021-11-27 22:02:32 +0300

    1

---
+++ pz_015/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace pz_015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  удалить строчку К из файла
+            int K = int.Parse(Console.ReadLine());
+
+            string file = @"C:\fet\piece.txt";
+
+            string[] filstrs = File.ReadAllLines(file);
+
+            if (K > filstrs.Length)
+            {
+                Console.WriteLine("nonono");
+            }
+            else
+            {
+                Console.WriteLine("\t\t\t\t" + filstrs[K]);
+
+                Array.Clear(filstrs, K, 1);
+                Array.Resize(ref filstrs, filstrs.Length - 1);
+
+                FileStream file0 = new FileStream(file, FileMode.Truncate);
+                StreamWriter writer = new StreamWriter(file0);
+
+                for (int i = 0; i < filstrs.Length; i++) writer.WriteLine(filstrs[i]);
+                writer.Close();
+            }
+        }
+    }
+}
---
+++ pz_016/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pz_016
+{
+    class Program
+    {
+        /*  Дано целое число K и текстовый файл. Удалить из файла строку с номером K. Если строки с
+            таким номером нет, то оставить файл без изменений.  */
+        static ulong Fak(ulong a)
+        {
+            if (a==0) return 0;
+            ulong result = 1;
+            for (ulong i = 1; i <=a; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            ulong result = 0;
+            ulong inputnumber = ulong.Parse(Console.ReadLine());
+            for (ulong i = 0; i <= inputnumber; i++)
+            {
+                result += Fak(i);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
