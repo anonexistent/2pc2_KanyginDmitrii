4d85338c7bab29fd93ee4c84ec5834aae9099515
fet <wwwzlx86@gmail.com>
2022-02-10 22:47:17 +0500

    1

--- pz_020/Program.cs
+++ pz_020/Program.cs
@@ -4,30 +4,16 @@ namespace pz_020
 {
     class Program
     {
-        static void book1()
-        {
-            BookCard a = new BookCard();
-            a.author = "fet a a";
-            a.condition = "gooooood(good)";
-            a.title = "cactus";
-            a.year = 1881;
-            a.id =10;
-            a.GetBookInfo();
-        }
-        static void book2()
-        {
-            BookCard a = new BookCard();
-            a.author = "qwe";
-            a.condition = "bad";
-            a.title = "bird";
-            a.year = 1999;
-            a.id = 11;
-            a.GetBookInfo();
-        }
         static void Main(string[] args)
         {
-            book1();
-            book2();
+            BookCard book1 = new BookCard();
+            BookCard book2 = new BookCard(11, "gweerhg","ertetwert",1999, "ewrgter");
+            BookCard book3 = new BookCard(-1,"2342");
+
+            book1.GetBookInfo();
+            book2.GetBookInfo();
+            book3.GetBookInfo();
+
         }
     }
 }
