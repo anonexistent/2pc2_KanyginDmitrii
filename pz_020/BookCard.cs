7e8fe62be2f0e506ad0f65db31c885b682ecf0be
fet <wwwzlx86@gmail.com>
2022-02-10 12:11:43 +0500

     

---
+++ pz_020/BookCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_020
+{ 
+    class BookCard
+    {
+        public ulong id;
+        public string author;
+        public string title;
+        public int year;
+        public string condition;
+        public void GetBookInfo()
+        {
+            Console.WriteLine($"BookInfo: \n id {id} \n author {author} \n title {title} \n year {year} \n condition {condition}");
+        }
+    }
+}
---
+++ pz_020/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pz_020
+{
+    class Program
+    {
+        static void book1()
+        {
+            BookCard a = new BookCard();
+            a.author = "fet a a";
+            a.condition = "gooooood(good)";
+            a.title = "cactus";
+            a.year = 1881;
+            a.id =10;
+            a.GetBookInfo();
+        }
+        static void book2()
+        {
+            BookCard a = new BookCard();
+            a.author = "qwe";
+            a.condition = "bad";
+            a.title = "bird";
+            a.year = 1999;
+            a.id = 11;
+            a.GetBookInfo();
+        }
+        static void Main(string[] args)
+        {
+            book1();
+            book2();
+        }
+    }
+}
