c62c73194c85c2e2badb4f297249da9a5c2c8bda
fet <wwwzlx86@gmail.com>
2022-02-17 01:09:12 +0300

    1

---
+++ pz_020/ConplexNumber.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_020
+{
+    class ConplexNumber
+    {
+        private float a;
+        private const char i = 'i';
+        private float b;
+        private ulong amount;
+        //private string negative_positive = $"{(a<0?"-":"+")}{}";
+
+        public ConplexNumber(float A, float B) {a=A; b=B; amount++; }
+        public ConplexNumber() { amount++; }
+
+        public static void GetConplexNumber(ConplexNumber qq)
+        {
+            
+            if (qq.a != 0 & qq.b != 0)
+                Console.WriteLine($"ConplexNumber: {qq.a}{(qq.b>0?'+':'-')}{Math.Abs(qq.b)}{i}");
+            else if (qq.a == 0 & qq.b != 0) Console.WriteLine($"ConplexNumber: {qq.b}{i}");
+            else if (qq.b == 0) Console.WriteLine($"Number: {qq.a}");
+        }
+
+        
+        public override string ToString()
+        {
+            return $"{a}{(b > 0 ? '+' : '-')}{Math.Abs(b)}{i}";
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(
+                $"numbers of conn numbers: {amount}\n"
+                //+
+                //$"\tof them {negative_positive} un_conplex"
+                );
+        }
+
+        //public static void Game()
+        //{
+        //    Console.CursorVisible = false;
+        //    int x = 20;
+        //    int y = 15;
+        //    ConsoleKeyInfo k;
+
+        //    Console.WriteLine("рисовать(Y/N): ");
+        //    bool painted = Console.ReadKey().Key == ConsoleKey.Y ? true : false;
+        //    Console.Clear();
+
+        //    do
+        //    {                
+        //        Console.SetCursorPosition(1, 1);
+        //        Console.WriteLine($"{x}\t{y}");
+        //        Console.SetCursorPosition(x, y);
+        //        Console.Write((char)3);
+        //        k = Console.ReadKey(true);
+
+        //        if (k.Key == ConsoleKey.W)
+        //            y--;
+        //        else if (k.Key == ConsoleKey.S)
+        //            y++;
+        //        else if (k.Key == ConsoleKey.A)
+        //            x--;
+        //        else if (k.Key == ConsoleKey.D)
+        //            x++;
+
+        //        if (painted == false) Console.Clear();
+        //    } while (k.Key != ConsoleKey.Escape);
+        //}
+    }
+}
--- pz_020/Program.cs
+++ pz_020/Program.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 
 namespace pz_020
-{
+{  
     class Program
-    {
+    {        
+        static void print(object a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(a);
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
-            BookCard book1 = new BookCard();
-            BookCard book2 = new BookCard(11, "gweerhg","ertetwert",1999, "ewrgter");
-            BookCard book3 = new BookCard(-1,"2342");
+            ConplexNumber one = new ConplexNumber(-3.99f, -5.0109f);
+            ConplexNumber two = new ConplexNumber(2,0);
+            ConplexNumber three = new ConplexNumber(0,-6);
 
-            book1.GetBookInfo();
-            book2.GetBookInfo();
-            book3.GetBookInfo();
+            ConplexNumber four = new ConplexNumber(-4.009284879105f, 9);
+            ConplexNumber five = new ConplexNumber(5, -10.995348957345f);
+            ConplexNumber six = new ConplexNumber();
 
+
+            ConplexNumber.GetConplexNumber(one);
+            ConplexNumber.GetConplexNumber(two);
+            ConplexNumber.GetConplexNumber(three);
+
+            ConplexNumber.GetConplexNumber(four);
+            ConplexNumber.GetConplexNumber(five);
+
+            ConplexNumber.GetConplexNumber(six);
+
+            print(five.ToString() + "\n");
+
+            
+
+            
         }
     }
 }
