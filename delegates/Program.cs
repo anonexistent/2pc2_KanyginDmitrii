e40dcda8430ae4b873ad81c79f935cbdb5e1b8ff
1 <1>
2022-04-14 13:31:01 +0500

    1

---
+++ delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+delegate char Delegate1();
+
+namespace delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoService obj1 = new AutoService("qwerty", true);
+            Console.WriteLine($"{obj1.meth()}");
+            AutoService obj10 = new AutoService("qwerty", false);
+            Console.WriteLine($"{obj10.meth()}");
+
+            AutoService obj2 = new AutoService("RUSSIA!!!", false);
+            Console.WriteLine($"{obj2.meth()}");
+            AutoService obj20 = new AutoService("RUSSIA!!!", true);
+            Console.WriteLine($"{obj20.meth()}");
+        }
+    }
+}
