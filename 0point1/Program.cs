74b687d6322adce094655d4b3cc5308db401af69
fet <wwwzlx86@gmail.com>
2022-05-04 19:56:22 +0500

    1

---
+++ 0point1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _0point1
+{
+    //???????      http://www.unicode.org/charts/PDF/UFFF0.pdf     ???????
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+
+            return -1;
+        }
+    }
+}
---
+++ pz_026_pointer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*12. Напишите программу, в которой через указатель реализуется подсчет количества спецсимволов в строке, 
+введенной пользователем.
+*/
+
+namespace pz_026_pointer
+{
+    //???????      http://www.unicode.org/charts/PDF/UFFF0.pdf     ???????
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = 0;
+
+            unsafe
+            {
+                fixed (char* x = a) for (int i = 0; i < a.Length; i++) if (!Char.IsLetter(x[i]) && !Char.IsDigit(x[i])) b++;
+            }
+
+            Console.WriteLine(b);
+        }
+    }
+}
